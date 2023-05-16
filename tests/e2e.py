4a33950151570eb2d9143c59747760c384b9a60e
Avi,Bernshtien <avi0610@gmail.com>
2023-05-16 05:38:16 +0300

    update

---
+++ tests/e2e.py
@@ -0,0 +1,29 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+
+SRV = Service("./tests/chromedriver.exe")
+test_driver = webdriver.Chrome(service=SRV)
+
+
+def test_scores_service(Leaderboard_url):
+    try:
+        test_driver.get(Leaderboard_url)
+        score = test_driver.find_element(By.XPATH, '//*[@id="wrapvalue"]').text
+        if int(score) in range(1, 1001):
+            return True
+        return False
+    except:
+        return False
+    finally:
+        test_driver.close()
+
+
+def main_function():
+    if test_scores_service("http://127.0.0.1:5000"):
+        return exit(0)
+    else:
+        return exit(1)
+
+
+print(main_function())
